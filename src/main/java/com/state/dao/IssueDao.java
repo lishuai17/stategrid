423c270b9f1170ddff25bb6c802e401c12924fab
wangkunbo3 <wangkunbo3@xdf.cn>
2016-05-07 19:06:30 +0800

    发布数据dao

--- src/main/java/com/state/dao/IssueDao.java
+++ src/main/java/com/state/dao/IssueDao.java
@@ -5,21 +5,37 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.state.po.DeclarePo;
+import com.state.po.ResultPo;
 
 public interface IssueDao {
 
-	/**
-	 * 根据ID查找申报主表是否存在
-	 * @param id
-	 * @return
-	 */
-	public int countDeclareById(long id);
+//	/**
+//	 * 根据ID查找结果数据是否存在
+//	 * @param id
+//	 * @return
+//	 */
+//	public int countResultById(long id);
+	
 	
 	/**
-	 * 根据参数查询申报主表
+	 * 插入结果数据  
+	 * @param ResultPo
+	 * @return 影响行数
+	 */
+	public long insertResult(ResultPo resultPo);
+	
+	
+	/**
+	 * 按地区查询结果中的所有单号
 	 * @param param
 	 * @return
 	 */
-	public List<DeclarePo> selectDeclareByParam(@Param("area")String area,@Param("mdate")String mdate
-			,@Param("mname")String mname);
+	public List<ResultPo> selectSheetOfResultByArea(@Param("area")String area,@Param("mdate")String mdate);
+	
+	/**
+	 * 根据参数查询结果数据
+	 * @param param
+	 * @return
+	 */
+	public List<ResultPo> selectResultByParam(@Param("area")String area,@Param("mdate")String mdate,@Param("mname")String mname);
 }
