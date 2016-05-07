f0e36d7e78dc81df955bc20b4660dc5b788b4b93
jh <jh@yangyan-PC>
2016-05-07 10:49:14 +0800

    hh

--- src/main/java/com/state/dao/IDeclareDataDao.java
+++ src/main/java/com/state/dao/IDeclareDataDao.java
@@ -1,9 +1,27 @@
 package com.state.dao;
 
+import java.util.Map;
+
+import com.state.po.DeclareDataPo;
+
 public interface IDeclareDataDao {
 	/**
 	 * 根据申报主表ID删除申报子表数据
 	 * @param declareId
 	 */
 	public void deleteDeclareData(Integer declareId);
+	
+	/**
+	 * 增加申请主表
+	 * @param declareDataDao
+	 */
+	public void insertDeclData(DeclareDataPo declareDataPo);
+	
+	
+	/**
+	 * 根据参数查找申请主表
+	 * @param declareDataDao
+	 */
+	public DeclareDataPo getDeclDataById(Map<String,String> param);
+	
 }
